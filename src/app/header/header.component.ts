0060194a8a32120e93c04aef6e10697afcca1f86
Nikunj Mochi <mochinikunj2@gmail.com>
2022-05-12 18:22:47 +0530

    Adding menu names for mobile devices

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from '../service/header.service';
-import { Language } from '../models/languages';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
