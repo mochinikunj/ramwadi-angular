03b13fde6ca032f2ac30fb41bab3138fa56e8e58
Nikunj Mochi <mochinikunj2@gmail.com>
2023-01-11 17:07:39 +0530

    updating drupal endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  drupalUrl: 'http://localhost/ramwadi-drupal',
+  drupalUrl: 'http://localhost/ramwadi-drupal/web',
   nodeUrl: 'http://localhost:3000',
   reCaptchaSiteKey: '6LdwzcIgAAAAAEK25kezKob-Tof7-CHvsTyauCnT'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  drupalUrl: 'http://localhost/ramwadi-drupal',
+  drupalUrl: 'http://localhost/ramwadi-drupal/web',
   nodeUrl: 'http://localhost:3000',
   reCaptchaSiteKey: '6LdnzcIgAAAAALC0WTRvMYWCgEgE-2itqqtQg6nK'
 };
